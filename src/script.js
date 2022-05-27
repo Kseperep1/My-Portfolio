a5328a7d0c1b944e8126a2223f6621a4f8005ec5
Kamogelo <kamogelo40@gmail.com>
2022-05-27 23:04:37 +0200

    Update Style Responsiveness

--- src/script.js
+++ src/script.js
@@ -1,23 +1,5 @@
 // Will launch when document is ready
 
-// const activeNav = () => {
-    // let hrefLocation = location.href;
-    // const navBar = document.querySelectorAll('a');
-    // const len = navBar.length;
-    // for (let i = 0; i < len; i++){
-    //     if (navBar[i].href === hrefLocation){
-    //         navBar[i].className = "active"
-    //     }   
-    // }
-
-    // for (let i = 0; i < navBar.length; i++){
-    //     links[i].addEventListener('click', () => {
-    //         let activeNav = document.getElementsByClassName('active');
-    //         activeNav[0].className = activeNav[0].className.replace("active");
-    //         this.className += "active"
-    //     })
-// }
-
 $(document).ready(function () {
 
     $('#nav-menu').click(function () {
@@ -29,20 +11,7 @@ $(document).ready(function () {
         $(this).removeClass('fa-times')
         $('header').removeClass('header-on')
     });
-    
-    // $('#nav-bar a').click(function () {
-    //     $(this).addClass('active').siblings().removeClass('active');
-    // });
-
-    // $('#nav-bar a').on('click', function (){
-    //     $(this).addClass('active').siblings().removeClass('active');
-        
-    // })
 
 });
 
 
-
-// $(document).on('click','#nav-bar ul li a', function () {
-//     $(this).addClass('active').siblings().removeClass('active')
-// });
