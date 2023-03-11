6e6437ee6273d187b65c450ddefe67a097aa9a1b
Kamogelo <kamogelo40@gmail.com>
2023-03-11 23:51:30 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,13 @@
 // Will launch when document is ready
 
 $(document).ready(function () {
+  $("#nav-menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("header-on");
+  });
 
-    $('#nav-menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('header-on');
-    });
-
-    $('#nav-menu').on('load scroll', function () {
-        $(this).removeClass('fa-times')
-        $('header').removeClass('header-on')
-    });
-
+  $("#nav-menu").on("load scroll", function () {
+    $(this).removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
 });
-
-
