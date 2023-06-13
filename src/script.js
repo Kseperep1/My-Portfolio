e6ff5ed91aed1ce892e51cf154f08efa8c46eddf
Kamogelo <kamogelo.seperepere@umuzi.org>
2023-06-13 11:29:45 +0200

    Updated Nav styles, & Nav buttons

--- src/script.js
+++ src/script.js
@@ -10,4 +10,34 @@ $(document).ready(function () {
     $(this).removeClass("fa-times");
     $("header").removeClass("header-on");
   });
+
+  $("#user").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
+
+  $("#aboutBtn").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
+
+  $("#careerBtn").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
+
+  $("#projectsBtn").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
+
+  $("#skillsBtn").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
+
+  $("#contactBtn").click(function () {
+    $("#nav-menu").removeClass("fa-times");
+    $("header").removeClass("header-on");
+  });
 });
